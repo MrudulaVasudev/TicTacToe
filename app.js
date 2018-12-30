449158517725dd60f6912587fa7f9da4335c2fa0
Mrudula Vasudev <mrudulavasudev02@gmail.com>
2018-12-30 16:03:35 +0530

    Modifying the code

--- app.js
+++ app.js
@@ -35,31 +35,40 @@ var successAlert = function(winner) {
 }
 
 var checkForWinner = function() {
-    var button1 = document.getElementById("1").value;
-    var button2 = document.getElementById("2").value;
-    var button3 = document.getElementById("3").value;
-    var button4 = document.getElementById("4").value;
-    var button5 = document.getElementById("5").value;
-    var button6 = document.getElementById("6").value;
-    var button7 = document.getElementById("7").value;
-    var button8 = document.getElementById("8").value;
-    var button9 = document.getElementById("9").value;
+    var bgColor = 'steelblue';
+    var button1 = document.getElementById("1");
+    var button2 = document.getElementById("2");
+    var button3 = document.getElementById("3");
+    var button4 = document.getElementById("4");
+    var button5 = document.getElementById("5");
+    var button6 = document.getElementById("6");
+    var button7 = document.getElementById("7");
+    var button8 = document.getElementById("8");
+    var button9 = document.getElementById("9");
 
-    if((button1 == "X" || button1 == "0") && (button1 == button2) && (button2 == button3)) {
+    if((button1.value == "X" || button1.value == "0") && (button1.value == button2.value) && (button2.value == button3.value)) {
+        button1.style.backgroundColor = button2.style.backgroundColor = button3.style.backgroundColor = bgColor;
         successAlert(button1);
-    } else if((button1 == "X" || button1 == "0") && (button1 == button4) && (button4 == button7)) {
+    } else if((button1.value == "X" || button1.value == "0") && (button1.value == button4.value) && (button4.value == button7.value)) {
+        button1.style.backgroundColor = button4.style.backgroundColor = button7.style.backgroundColor = bgColor;
         successAlert(button1);
-    } else if((button3 == "X" || button3 == "0") && (button3 == button6) && (button6 == button9)) {
+    } else if((button3.value == "X" || button3.value == "0") && (button3.value == button6.value) && (button6.value == button9.value)) {
+        button3.style.backgroundColor = button6.style.backgroundColor = button9.style.backgroundColor = bgColor;
         successAlert(button3);
-    } else if((button7 == "X" || button7 == "0") && (button7 == button8) && (button8 == button9)) {
+    } else if((button7.value == "X" || button7.value == "0") && (button7.value == button8.value) && (button8.value == button9.value)) {
+        button7.style.backgroundColor = button8.style.backgroundColor = button9.style.backgroundColor = bgColor;
         successAlert(button7);
-    } else if((button2 == "X" || button2 == "0") && (button2 == button5) && (button5 == button8)) {
+    } else if((button2.value == "X" || button2.value == "0") && (button2.value == button5.value) && (button5.value == button8.value)) {
+        button2.style.backgroundColor = button5.style.backgroundColor = button8.style.backgroundColor = bgColor;
         successAlert(button2);
-    } else if((button4 == "X" || button4 == "0") && (button4 == button5) && (button5 == button6)) {
+    } else if((button4.value == "X" || button4.value == "0") && (button4.value == button5.value) && (button5.value == button6.value)) {
+        button4.style.backgroundColor = button5.style.backgroundColor = button6.style.backgroundColor = bgColor;
         successAlert(button4)
-    } else if((button1 == "X" || button1 == "0") && (button1 == button5) && (button5 == button9)) {
+    } else if((button1.value == "X" || button1.value == "0") && (button1.value == button5.value) && (button5.value == button9.value)) {
+        button1.style.backgroundColor = button5.style.backgroundColor = button9.style.backgroundColor = bgColor;
         successAlert(button1);
-    } else if((button3 == "X" || button3 == "0") && (button3 == button5) && (button5 == button7)) {
+    } else if((button3.value == "X" || button3.value == "0") && (button3.value == button5.value) && (button5.value == button7.value)) {
+        button3.style.backgroundColor = button5.style.backgroundColor = button7.style.backgroundColor = bgColor;
         successAlert(button3);
     }
 }
@@ -156,6 +165,15 @@ var reset = function() {
     button7.value = ' ';
     button8.value = ' ';
     button9.value = ' ';
+    button1.style.backgroundColor='darkseagreen';
+    button2.style.backgroundColor='darkseagreen';
+    button3.style.backgroundColor='darkseagreen';
+    button4.style.backgroundColor='darkseagreen';
+    button5.style.backgroundColor='darkseagreen';
+    button6.style.backgroundColor='darkseagreen';
+    button7.style.backgroundColor='darkseagreen';
+    button8.style.backgroundColor='darkseagreen';
+    button9.style.backgroundColor='darkseagreen';
     button1.disabled = false;
     button2.disabled = false;
     button3.disabled = false;
@@ -186,6 +204,7 @@ var popupDismiss = function() {
 }
 
 var buttonClick = function (number) {
+    var bgColor;
     if (value === "X") {
         value = "0";
         checkNumber(number, value);
